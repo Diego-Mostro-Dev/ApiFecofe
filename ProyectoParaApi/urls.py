1d7631744bd325fa454ddf0481ea269a664e600a
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2025-01-21 19:35:09 -0300

    Subiendo el proyecto ApiFecofe evitar el favicon

--- ProyectoParaApi/urls.py
+++ ProyectoParaApi/urls.py
@@ -1,18 +1,3 @@
-"""ProyectoParaApi URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -22,18 +7,13 @@ from django.http import HttpResponse
 router = DefaultRouter()
 router.register(r'productos', ProductoViewSet)
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-]
-
-
-
-
+# Función para manejar la solicitud del favicon
 def favicon(request):
     return HttpResponse(status=204)  # Respuesta vacía para evitar el error 400
 
+# Combina las rutas en una sola lista
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('favicon.ico', favicon),  # Ruta para favicon
-    # otras rutas...
 ]
